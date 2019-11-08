69c8bea5d08d27a16e7aaba35547694612af35a5
jt <jt@TRLDSC1.tch.harvard.edu>
2019-11-08 11:13:24 -0500

    Updates

---
+++ precache-manifest.86fe414afaf69573ea706605afafd10e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "63261f941677dd3c95a015f0c163efb8",
+    "url": "/react-scatter-plot/index.html"
+  },
+  {
+    "revision": "a686e41154fd92225c54",
+    "url": "/react-scatter-plot/static/js/2.5bfccd3c.chunk.js"
+  },
+  {
+    "revision": "eac059054a932ddb85d2",
+    "url": "/react-scatter-plot/static/js/main.088f4ac7.chunk.js"
+  },
+  {
+    "revision": "37a993d1ced838e3ccc0",
+    "url": "/react-scatter-plot/static/js/runtime~main.7ffd987a.js"
+  }
+]);
