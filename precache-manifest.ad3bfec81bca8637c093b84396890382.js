ce0f7e1f00ba43ac9866baa27ba5e61b9f30cf6b
J Tan <jt234wq@gmail.com>
2020-01-01 01:11:21 -0500

    Updates

---
+++ precache-manifest.ad3bfec81bca8637c093b84396890382.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6bb51b73fd7a33a3de6cf7a5d613a5a2",
+    "url": "/react-scatter-plot/index.html"
+  },
+  {
+    "revision": "5f6717934bc68e4f751f",
+    "url": "/react-scatter-plot/static/js/2.16ea9d73.chunk.js"
+  },
+  {
+    "revision": "67935b6d863b3e23bc08cf31dbc40ecc",
+    "url": "/react-scatter-plot/static/js/2.16ea9d73.chunk.js.LICENSE"
+  },
+  {
+    "revision": "c158f5b723c17227894c",
+    "url": "/react-scatter-plot/static/js/main.0b508d95.chunk.js"
+  },
+  {
+    "revision": "11d3797e280b2b2acaf8",
+    "url": "/react-scatter-plot/static/js/runtime-main.5ae47f0c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/react-scatter-plot/precache-manifest.86fe414afaf69573ea706605afafd10e.js"
+  "/react-scatter-plot/precache-manifest.ad3bfec81bca8637c093b84396890382.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/react-scatter-plot/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
